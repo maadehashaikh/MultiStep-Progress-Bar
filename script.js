f8e2a55ef702b114f8e89d1bb1a812368b68f050
Maadeha shaikh <maadeham@gmail.com>
2024-05-29 22:04:25 +0500

    update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let progress = document.getElementById("progress");
 let nextBtn = document.getElementById("next");
 let prevBtn = document.getElementById("prev");
+const progressStep = document.querySelectorAll(".progress-step .fa-solid");
+let currentStep = 1;
