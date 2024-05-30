d24f85c0907a850dee45b09782a5b4bb1478984f
Maadeha shaikh <maadeham@gmail.com>
2024-05-30 21:50:36 +0500

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let progress = document.getElementById("progress");
 let nextBtn = document.getElementById("next");
 let prevBtn = document.getElementById("prev");
-const progressStep = document.querySelectorAll(".progress-step .fa-solid");
+const progressSteps = document.querySelectorAll(".progress-step .fa-solid");
+// default step value !! 
 let currentStep = 1;
+const next = () => {
+  currentStep++;
+  refresh();
+  console.log("I am next");
+}
+const prev =  ()  => {
+  currentStep--;
+  refresh();
+  console.log("I am prev");
+}
+
+const refresh = () => {
+  progressSteps.forEach((step , index) => {
+     if(index < currentStep)  step.classList.add("active");
+     else step.classList.remove("active");
+    });
+  }
+
